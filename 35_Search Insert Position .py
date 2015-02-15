18c5d8b3d0ea47cae50d1b6e9b232fa727f98cce
ljcbeyond <lijunchengbeyond@gmail.com>
2015-02-15 23:37:53 +0800

    add comment

--- 35_Search Insert Position .py
+++ 35_Search Insert Position .py
@@ -16,11 +16,12 @@ class Solution:
     # @param target, an integer to be inserted
     # @return integer
     def searchInsert(self, A, target):
-        try:
+        try:#to see if the target is in A,if it is,just return
             return A.index(target)
         except ValueError:
-            if target< A[0]:return 0
+            # find the limit element of the target in the A list
             for idx,num in enumerate(A):
                 if num>target:
                     return idx
+            # if target is bigger than anyone in A,return the length of list
             return idx+1
